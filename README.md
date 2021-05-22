ff750e1ae52b58f2c65cd9f7c38903c8358fd5e8
alessandroprudencio <alessandroconectado@gmail.com>
2021-05-22 14:53:31 -0400

    feat: update readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-Criando um crud de produtos utilizando a arquitetura hexagonal com go lang
+Crud de produtos utilizando a arquitetura hexagonal com go lang
+
+Comandos via CLI 
+
+Flags:
+
+``-a, --action string    Enable /  Disable a product (default "enable")``
+
+``-h, --help             help for cli``
+
+``-i, --id string        Product ID``
+
+``-p, --price float      Product price``
+
+``-n, --product string   Product name``
+    
+Uso:
+
+Cadastrar Produto
+
+    go run main.go cli -a=create -n=Product cli -p=15.00
+
+Buscar Produto
+
+    go run main.go cli -a=get -i=edc49fdd-f5f3-4dca-a4a4-8ffcfd744740
--- cmd/cli.go
+++ cmd/cli.go
@@ -16,6 +16,7 @@ limitations under the License.
 package cmd
 
 import (
+    "fmt"
 	"github.com/spf13/cobra"
     _ "github.com/mattn/go-sqlite3"
     cli "github.com/alessandroprudencio/Go-Hexagonal/adapters/cli"
@@ -37,7 +38,11 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		cli.Run(&productService, action, productId, productName, productPrice)
+	    res, err :=	cli.Run(&productService, action, productId, productName, productPrice)
+	    if err != nil {
+	        fmt.Println(err.Error())
+	    }
+	    fmt.Println(res)
 	},
 }
 
